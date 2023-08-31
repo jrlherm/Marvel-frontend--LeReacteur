34613b892e5317071806ea498edc43380d30d44b
JR Lherm <lherm.jr@gmail.com>
2023-08-31 15:20:49 +0200

    implementing search

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -5,12 +5,13 @@ import { Link } from "react-router-dom";
 const Characters = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://site--marvel-backend--vm2w9vyj7r62.code.run/characters"
+          `https://site--marvel-backend--vm2w9vyj7r62.code.run/characters/?name=${searchQuery}`
         );
         console.log("data ==> ", response.data);
         setData(response.data);
@@ -20,7 +21,7 @@ const Characters = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [searchQuery]);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -28,22 +29,28 @@ const Characters = () => {
 
   return (
     <div className="characters-list">
-      <div className="container">
-        <h1>Personnages</h1>
-        <div className="list">
-          {data?.results?.map((character) => (
-            <Link to={`/character/${character._id}`} key={character._id}>
-              <div className="item">
-                <img
-                  src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                  alt={`${character.name} thumbnail`}
-                />
-                <h3>{character.name}</h3>
-                <p>{character.description}</p>
-              </div>
-            </Link>
-          ))}
-        </div>
+      {/* <div className="container"> */}
+      <h1>Personnages</h1>
+      <input
+        type="text"
+        placeholder="Search characters"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <div className="list">
+        {data?.results?.map((character) => (
+          <Link to={`/character/${character._id}`} key={character._id}>
+            <div className="item">
+              <img
+                src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                alt={`${character.name} thumbnail`}
+              />
+              <h3>{character.name}</h3>
+              <p>{character.description}</p>
+            </div>
+          </Link>
+        ))}
+        {/* </div> */}
       </div>
     </div>
   );
