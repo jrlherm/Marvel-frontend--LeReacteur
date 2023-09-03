299fe1eaae8e37624f8b3d3f396276af5587de5a
JR Lherm <lherm.jr@gmail.com>
2023-09-03 23:23:27 +0200

    updated homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div
+      className="home"
+      style={{
+        height: "87vh",
+        backgroundImage: `url(${"https://www.komar.de/media/cms/fileadmin/user_upload/Category/Fototapeten/Marvel/komar-fototapeten-marvel.jpg"})`,
+        backgroundSize: "cover",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        color: "#FFF",
+      }}
+    >
+      <h1>Welcome to marvel explorer</h1>
+      <div className="buttons">
+        <div className="homelink">
+          <Link to="/characters">Explore characters</Link>
+        </div>
+        <div className="homelink">
+          <Link to="/characters">Explore Comics</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
